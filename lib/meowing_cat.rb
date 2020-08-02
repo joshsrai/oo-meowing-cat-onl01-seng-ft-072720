3e9b72c7a4e18bba9ca6ddcf72d5d7ee042c7fee
Josh Raiborde <joshsrai@yahoo.com>
2020-08-02 15:31:00 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -2,3 +2,20 @@
 class Cat
   attr_accessor :name
 end
+
+
+
+class Dog 
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  def name 
+    @name
+  end
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end
+  def breed 
+    @breed
+  end
+end
